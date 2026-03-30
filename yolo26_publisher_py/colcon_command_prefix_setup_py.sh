6f7ff7926d3483af68adc56ed78f878853b45692
merveakbey <114735328+merveakbey@users.noreply.github.com>
2026-03-30 10:13:47 +0300

    Delete yolo26_publisher_py directory

--- yolo26_publisher_py/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/merve/ros2_ws/install/yolo26_interfaces/share/yolo26_interfaces/package.sh"
--- yolo26_publisher_py/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/merve/ros2_ws/install/yolo26_publisher_py'
