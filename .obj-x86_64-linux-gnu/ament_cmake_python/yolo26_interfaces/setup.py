c44fcdded27325f5e5526f9db4118159e5312d48
merveakbey <114735328+merveakbey@users.noreply.github.com>
2026-03-31 11:00:55 +0300

    Delete .obj-x86_64-linux-gnu directory

--- .obj-x86_64-linux-gnu/ament_cmake_python/yolo26_interfaces/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='yolo26_interfaces',
-    version='0.0.1',
-    packages=find_packages(
-        include=('yolo26_interfaces', 'yolo26_interfaces.*')),
-)
